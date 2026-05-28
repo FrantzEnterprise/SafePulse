6768d0a59b5557d70e1a07a7be3e25fdd71d4521
Safe Technician <safe-technician@safeservice.dev>
2026-05-28 21:56:31 +0000

    v0.10.84: New JS hash, force cache refresh

--- skills/safepulse/src/index.jsx
+++ skills/safepulse/src/index.jsx
@@ -3,4 +3,9 @@ import { createRoot } from "react-dom/client";
 import SafePulseDemo from "./SafePulseDemo.jsx";
 import "./style.css";
 
-createRoot(document.getElementById("root")).render(<SafePulseDemo />);
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  document.body.innerHTML = '<div style="color:red;padding:40px">Error: root element not found</div>';
+} else {
+  createRoot(rootElement).render(<SafePulseDemo />);
+}
